ffca4dccd8b6e81f3371ca331b0854345a97e77f
Rok Šekoranja <45943664+Rookxc@users.noreply.github.com>
2020-06-28 17:42:03 +0200

    Added button to App.js

--- App.js
+++ App.js
@@ -1,31 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
 
-export default function UselessTextInput() {
+export default function Login() {
 
   //const [value, onChangeText] = React.useState('Text 1');
   //const [value2, onChangeText2] = React.useState('Text 2');
 
   return (
     <React.Fragment>
-      
+
       <View style={styles.container}>
-        <Text style={styles.SignIn}> Sign In PLS WORK!</Text>
+        <Text style={styles.SignIn}> Sign In</Text>
         <StatusBar style="auto" />
       </View>
-     
-      <TextInput style={styles.txtEmail} 
-         //INSERT YOUR CODE FOR TEXTBOX HERE
-          //onChangeText={text => onChangeText(text)}
-         //value={value}
+
+      <TextInput style={styles.txtEmail}
+      //INSERT YOUR CODE FOR TEXTBOX HERE
+      //onChangeText={text => onChangeText(text)}
+      //value={value}
       />
 
-      <TextInput secureTextEntry={true} style={styles.txtPassword} 
-         //INSERT YOUR CODE FOR TEXTBOX HERE
-         //onChangeText2={text => onChangeText(text)}
-         //value2={value2}
+      <TextInput secureTextEntry={true} style={styles.txtPassword}
+      //INSERT YOUR CODE FOR TEXTBOX HERE
+      //onChangeText={text => onChangeText(text)}
+      //value={value}
       />
+
+      <TouchableOpacity style={styles.btnContainer}>
+        <Text style={styles.btnText}>Sign in</Text>
+      </TouchableOpacity>
+
+
     </React.Fragment>
 
   );
@@ -34,7 +40,7 @@ export default function UselessTextInput() {
 const styles = StyleSheet.create({
   container: {
     //flex: 1,
-    marginTop: 300,
+    marginTop: 275,
     marginLeft: 50,
     marginRight: 50,
 
@@ -45,17 +51,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  SignIn:{
-    fontSize: 20,
+  SignIn: {
+    fontSize: 50,
     fontWeight: "bold"
   },
 
-  txtEmail:{
+  txtEmail: {
     //Text align:
     paddingLeft: 20,
 
     //Textbox align:
-    marginTop: 80,
+    marginTop: 54,
     marginLeft: 50,
     marginRight: 50,
     height: 40,
@@ -71,7 +77,7 @@ const styles = StyleSheet.create({
     elevation: 7,
   },
 
-  txtPassword:{
+  txtPassword: {
     //Text align:
     paddingLeft: 20,
 
@@ -91,4 +97,23 @@ const styles = StyleSheet.create({
     shadowRadius: 4.65,
     elevation: 7,
   },
+
+  btnContainer: {
+    marginTop: 40,
+    marginLeft: 50,
+    marginRight: 50,
+
+    elevation: 8,
+    backgroundColor: "#009688",
+    borderRadius: 25,
+    paddingVertical: 10,
+    paddingHorizontal: 12
+  },
+  btnText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  }
 });
